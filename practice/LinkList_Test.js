de1c3c52eb57256c6dd3f81f3202aa10623567e3
y0dev <devdoesit17@gmail.com>
2021-02-19 12:36:43 -0800

    update LinkList Class

--- practice/LinkList_Test.js
+++ practice/LinkList_Test.js
@@ -1,10 +1,13 @@
 const {LinkList} = require('../lib/LinkList');
 
 const list = new LinkList();
-list.addToBack(4);
-list.addToBack(1);
-list.addToBack(5);
+list.append(4);
+list.append(1);
+list.append(5);
 list.addToFront(10);
 list.print();
 list.removeEnd();
+list.print();
+list.insertAt(1,2);
+list.insertAt(0,2);
 list.print();
