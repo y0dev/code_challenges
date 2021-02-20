dee97dc3db1ae31cbf7a051950f54f75c1dbdce6
y0dev <devdoesit17@gmail.com>
2021-02-19 21:20:05 -0800

    binary tree adding

---
+++ practice/BinarySearchTree_Test.js
@@ -0,0 +1,14 @@
+const { BinarySearchTree } = require('../lib/BinarySearchTree');
+
+const bst = new BinarySearchTree();
+bst.insert(20);
+bst.insert(24);
+bst.insert(7);
+bst.insert(15);
+bst.insert(5);
+bst.insert(3);
+bst.insert(9);
+bst.insert(8);
+bst.insert(10);
+bst.insert(13);
+bst.insert(12);
