09d228345f2686324fe64f41a759d06e1ab638aa
y0dev <devdoesit17@gmail.com>
2021-02-25 20:44:39 -0800

    grid_paths added to recursion practice

--- practice/recursive.js
+++ practice/recursive.js
@@ -32,3 +32,39 @@ console.log(sum(4));
 console.log(sum(5));
 console.log(sum(14));
 
+/* 
+ * Write a function that takes to inputs n and m
+ * and outputs the number of unique paths from
+ * top left corner to bottom right corner of a
+ * n x m grid.
+ * 
+ * Constraints: You can only move down or 
+ * right 1 unit at a time
+ *
+ * Ex : (n = 2, m = 4): grid_paths = 4
+ *      (n = 0, m = 0): grid_paths = 0
+ *      (n = 1, m = 0): grid_paths = 1
+ *      (n = 2, m = 0): grid_paths = 1
+ *      (n = 0, m = 1): grid_paths = 1
+ *      (n = 0, m = 2): grid_paths = 1
+ *      (n = 1, m = 1): grid_paths = 1
+ *      (n = 1, m = 2): grid_paths = 1
+ *      (n = 2, m = 2): grid_paths = 2
+ *      (n = 2, m = 3): grid_paths = 3
+ *      (n = 2, m = 4): grid_paths = 4
+ *      (n = 3, m = 2): grid_paths = 3
+ *      (n = 4, m = 2): grid_paths = 4
+ * 
+ */ 
+
+ var grid_paths = (n,m) => {
+     // Base Case
+     if( n == 1 || m == 1)
+        return 1;
+    else
+        return grid_paths(n - 1, m) + grid_paths(n, m - 1);
+ }
+
+console.log(grid_paths(2,4));
+console.log(grid_paths(1,5));
+console.log(grid_paths(3,3));
